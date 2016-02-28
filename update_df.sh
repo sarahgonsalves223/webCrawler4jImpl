e6d6b9ccfd5ab564c0dd70c7f7a8ea4232006cda
Sarah Gonsalves <sarahgonsalves223@gmail.com>
2016-02-27 17:25:20 -0800

    Update update_df.sh

--- update_df.sh
+++ update_df.sh
@@ -5,6 +5,6 @@ while(i<array.length){
 var term_name = array[i];
 var id = db.new_inverted_index.find({term:term_name}).toArray(); //gets the record for every term
 var df_count = id[0].docs.length; //calculates the length of "docs" for every term
-db.new_inverted_index.update({term:term_name},{$set:{df:df_count}}) //inserts df for every record
+db.new_inverted_index.update({term:term_name},{$set:{df:df_count}}); //inserts df for every record
 i = i+1;
 }
